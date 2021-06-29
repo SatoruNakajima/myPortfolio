a5a14e744eb92e5b3a404e201f7a2479c3084775
Tomokazu_Nakajima <tomokazu04929@gmail.com>
2021-06-29 11:02:00 +0900

    トップページ編集

--- js/main.js
+++ js/main.js
@@ -20,3 +20,15 @@ $(document).ready(function() {
 		anchors: ['top_fv', 'top_portfolio', 'top_about', 'top_contact']
 	});
 });
+
+// スライダー
+// トップページポートフォリオ
+$(document).ready(function(){
+  $('.top_portfolio_slides').slick({
+    autoplay: true,
+		autoplaySpeed: 3000,
+		arrows: true,
+    prevArrow: '<button type="button"  aria-lavel="Previous" class="top_portfolio_prev slide_arrow">prev</button>',
+    nextArrow: '<button type="button" aria-label="Next" class="top_portfolio_next slide_arrow">next</button>',
+  });
+});
