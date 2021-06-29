f6e2c67cd69fd9c6ce1c1d9f4e6b02da039e2d33
Tomokazu_Nakajima <tomokazu04929@gmail.com>
2021-06-29 15:11:36 +0900

    トップページスライダー編集

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var parallaxInstance = new Parallax(bg, {
 });
 
 // top_fv
-var top_fv = document.getElementById('top_fv');
+var top_fv = document.getElementById('top_fv_parallax');
 var parallaxInstance = new Parallax(top_fv, {
   relativeInput: true
 });
@@ -26,7 +26,7 @@ $(document).ready(function() {
 $(document).ready(function(){
   $('.top_portfolio_slides').slick({
     autoplay: true,
-		autoplaySpeed: 3000,
+		autoplaySpeed: 3700,
 		arrows: true,
     prevArrow: '<button type="button"  aria-lavel="Previous" class="top_portfolio_prev slide_arrow">prev</button>',
     nextArrow: '<button type="button" aria-label="Next" class="top_portfolio_next slide_arrow">next</button>',
